b0785979accd042ff90f391a9ebafed75959fae6
Gamage Hashan <tharinduh20@gmail.com>
2026-03-21 14:27:07 +0530

    Initialize Express server with database connection, CORS, and API routes for authentication, products, and sales.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db";
+
 import authRoutes from "./routes/authRoutes";
 import productRoutes from "./routes/productRoutes";
 import saleRoutes from "./routes/saleRoutes";
@@ -10,25 +11,27 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+
 app.use(cors({
-    origin: process.env.NODE_ENV === 'production'
-        ? ['https://tee-kp9f5wvr3-hashans-projects-41040970.vercel.app/login', 'https://tee-pos-backend.vercel.app']
-        : '*',
+    origin: process.env.NODE_ENV === "production"
+        ? "https://tee-kp9f5wvr3-hashans-projects-41040970.vercel.app"
+        : "*",
     credentials: true,
 }));
+
+
 app.use(express.json());
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/sales", saleRoutes);
 
+
 app.get("/", (req, res) => {
     res.send("POS API Running");
 });
+
+
 export default app;
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
